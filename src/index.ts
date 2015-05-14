117a1ffc4d6019e01e4486a8609478bcff6f7457
Matias Emanuel Surdi <matiassurdi@gmail.com>
2015-05-14 08:38:57 +0200

    Basic scaffolding

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,10 @@ function loadMetadata():any {
 program
     .version(loadMetadata().version)
     .usage('[options] -- <command>')
-    .option('-p --path <path>', 'Path to cache')
+    .option('-p --path <path>', 'Path to cache', '.')
     .option('-k --key <key>', 'Cache key')
-    .option('-c --cache-path <path>', 'Path to use for cache storage')
-    .option('-r --remove-older <time>', 'Remove cache entries older than <time>')
+    .option('-c --cache-dir <path>', 'Path to use for cache storage', '~/.pcache')
+    .option('-r --remove-older <time>', 'Remove cache entries older than <time>. Set to 0 to cache forever', 0)
     .on('--help', function () {
         console.log('  Examples:');
         console.log('');
@@ -24,4 +24,56 @@ program
         console.log('');
     });
 
-program.parse(process.argv);
+class Cache {
+    constructor(private dir:Path);
+
+    set(key:string, sourceDir:Path):void {
+
+    }
+
+    get(key:string, targetDir:Path):Path {
+
+    }
+
+    del(key:string):void {
+
+    }
+
+    list():Path[] {
+
+    }
+
+    has(key:string):boolean {
+
+    }
+
+}
+
+interface Path extends path.ParsedPath {
+}
+
+function main() {
+    let key:string = program.optionFor('key').toString();
+    let cacheDir:Path = path.parse(program.optionFor('cache-dir').toString());
+    let sourcePath:Path = path.parse(program.optionFor('path').toString());
+    let command:string = program.args.join(' ');
+
+    program.parse(process.argv);
+    console.log(program.args);
+    if (!key) {
+        process.exit(1);
+    }
+
+    let cache = new Cache(cacheDir);
+
+    if (cache.has(key)) {
+        cache.get(key, sourcePath)
+    } else {
+        run(command);
+        cache.set(key, sourcePath);
+    }
+
+}
+
+
+main();
