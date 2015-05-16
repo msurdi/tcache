d262c49189ed89d66fae04e060aad46efccd3e8f
Matias Emanuel Surdi <matiassurdi@gmail.com>
2015-05-16 10:55:47 +0200

    Implement core logic

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ require('source-map-support').install();
 import program = require('commander');
 import fs = require('fs');
 import path = require('path');
+import child_process = require('child_process');
 
 function loadMetadata():any {
     var appDir = path.dirname(require.main.filename);
@@ -14,7 +15,7 @@ program
     .version(loadMetadata().version)
     .usage('[options] -- <command>')
     .option('-p --path <path>', 'Path to cache', '.')
-    .option('-k --key <key>', 'Cache key')
+    .option('-k --key <key>', 'Cache key. By default, the path basename is used')
     .option('-c --cache-dir <path>', 'Path to use for cache storage', '~/.pcache')
     .option('-r --remove-older <time>', 'Remove cache entries older than <time>. Set to 0 to cache forever', 0)
     .on('--help', function () {
@@ -25,52 +26,89 @@ program
     });
 
 class Cache {
-    constructor(private dir:Path);
-
-    set(key:string, sourceDir:Path):void {
+    constructor(private dir:string) {
 
     }
 
-    get(key:string, targetDir:Path):Path {
+    set(key:string, sourceDir:string, cb?:Function):void {
+        console.log('Saving to cache');
+        return callback(cb);
 
     }
 
-    del(key:string):void {
+    get(key:string, targetDir:string, cb?:Function):void {
+        console.log('Loading from cache');
+        return callback(cb);
 
     }
 
-    list():Path[] {
+    del(key:string, cb?:Function):void {
+        console.log('Deleting from cache');
+        return callback(cb);
+    }
+
+    list(cb?:((results:string) => void)) {
+        return callback(cb);
 
     }
 
-    has(key:string):boolean {
-
+    has(key:string, cb?:((exists:boolean) => void)) {
+        console.log('has cache?');
+        return callback(cb);
     }
 
 }
 
-interface Path extends path.ParsedPath {
+function callback(fn:Function, ...args:any[]) {
+    if (typeof fn === 'function') {
+        return fn.apply(null, Array.prototype.slice.call(arguments, 1))
+    }
 }
+
 
 function main() {
-    let key:string = program.optionFor('key').toString();
-    let cacheDir:Path = path.parse(program.optionFor('cache-dir').toString());
-    let sourcePath:Path = path.parse(program.optionFor('path').toString());
-    let command:string = program.args.join(' ');
-
     program.parse(process.argv);
-    console.log(program.args);
-    if (!key) {
-        process.exit(1);
-    }
-
+    debugger;
+    let opts = program.opts();
+    let cacheDir = opts['cacheDir'].toString();
+    let sourcePath = opts['path'].toString();
+    let key = opts['key'] ? opts['key'].toString() : path.basename(sourcePath.toString());
+    let command = program.args ? program.args.join(' ') : undefined;
     let cache = new Cache(cacheDir);
 
-    if (cache.has(key)) {
-        cache.get(key, sourcePath)
-    } else {
-        run(command);
-        cache.set(key, sourcePath);
+    cache.has(key, (exists) => {
+        if (exists) {
+            cache.get(key, sourcePath, afterCacheGet);
+        }
+        else {
+            if (command) {
+                run(command, afterRun);
+            }
+        }
+    });
+
+    function run(command, cb?:(error:Error, stdout:Buffer, stderr:Buffer) =>void) {
+        console.log('Running command');
+        let options = {
+            env: process.env,
+            shell: process.env['SHELL'],
+        };
+        child_process.exec(command, options, afterRun)
+    }
+
+    function afterCacheGet() {
+        console.log(`Restored ${sourcePath} from cache`);
+    }
+
+    function afterRun(error:Error, stdout:Buffer, stderr:Buffer) {
+        if (error) {
+            console.log(`pcache command for generating files failed with exit status: ${error['code']}`)
+        }
+        cache.set(key, sourcePath, afterCacheSet);
+    }
+
+    function afterCacheSet() {
+        console.log(`Saved ${sourcePath} to cache`);
     }
 
 }
